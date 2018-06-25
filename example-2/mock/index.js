b1b711709c38e4f5c618b4d0c7b8824a1565cdfc
shao.zhang <zhangyishao@peilian.com>
2018-06-25 16:10:20 +0800

    expess replace import to require

--- example-2/mock/index.js
+++ example-2/mock/index.js
@@ -1,11 +1,11 @@
 // mock入口，该代码结构可最大程度防止冲突
-import index from './data'
+const index = require('./data')
 
 let container = [
     index
 ];
 
-export default {
+module.exports = {
     init() {
         for(let i of container) {
             i.init();
